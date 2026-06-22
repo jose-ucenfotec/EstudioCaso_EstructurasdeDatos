c90051f0075daa06d090553bc2a15f254222287d
jose-ucenfotec <jbarrantessa@ucenfotec.ac.cr>
2026-06-21 21:42:01 -0600

    deputando main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import java.io.IOException;
 
-/*
- * Clase Main
- * Punto de entrada del programa. Crea el menu y lo muestra al usuario.
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
